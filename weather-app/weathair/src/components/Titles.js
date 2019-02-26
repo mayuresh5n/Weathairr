8caaa5acf3c160087c65ae35eaa2c97f4d3b7f4f
mayureshnazare <35481672+mayureshnazare@users.noreply.github.com>
2019-02-26 12:56:03 -0800

    React app to check local weather

---
+++ weather-app/weathair/src/components/Titles.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Titles=()=>(
+    <div>
+            <h1 className="title-container__title">WeathAir for you</h1>
+            <p className="title-container__subtitle">Find weathAir of your city</p>
+            </div>
+);
+export default Titles;
